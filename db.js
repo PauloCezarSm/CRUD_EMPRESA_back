6815d4aca40ef3dcf7634c6588322ba2060e5086
Paulo Cezar Sales de Melo <104694898+PauloCezarSm@users.noreply.github.com>
2023-09-09 11:48:02 -0300

    Update db.js

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ import mysql from "mysql";
 export const db = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "Teste@123",
-    database: "crud",
+    password: "sua_senha_bd",
+    database: "sua_data_bd",
 });
 
